176960d54b81e56de12487c56e326bed88e093dd
Sean <Sean4.Taylor@live.uwe.ac.uk>
2016-02-26 15:31:03 +0000

    Split boss into two classes
    Added Boss Manager script
    Added Golem script

---
+++ Assets/Scripts/Boss/Boss Manager.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class BossManager : MonoBehaviour {
+
+	public Animator leftArm, rightArm;
+
+	public static float health = 100;
+	
+	public bool alive
+	{
+		get
+		{
+			return health > 0;
+		}
+	}
+	public float currentHealth;
+	public Image healthBar;
+
+	List<float> healthRecord;
+
+	public List<SpriteRenderer> bossParts;
+
+	public List<Sprite> headStages, chestStages, lArmStages, rArmStages, c2, c3, c4;
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+		#region Debug options for testing
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			health = 100;
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			health = 70;
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			health = 50;
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha4))
+		{
+			health = 30;
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha5))
+		{
+			health = 10;
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha6))
+		{
+			health = 0;
+		}
+		#endregion 
+
+		#region Stadge select (case?)
+
+		if (health > 80)
+		{
+			StadgeOne ();
+		}
+		else if(health < 80 && health > 60)
+		{
+			StadgeTwo ();
+		}
+		else if(health < 60 && health > 40)
+		{
+			StadgeThree ();
+		}
+		else if(health < 40 && health > 20)
+		{
+			StadgeFour ();
+		}
+		else if(health <= 0)
+		{
+			StadgeFive ();
+		}
+		#endregion
+
+		currentHealth = health;
+
+		if (!alive)
+		{
+			Detach(gameObject);
+	    }
+	}
+
+	public abstract void StadgeOne ();
+	public abstract void StadgeTwo ();
+	public abstract void StadgeThree ();
+	public abstract void StadgeFour ();
+	public abstract void StadgeFive ();
+
+	public abstract void SetBossParts ();
+
+	void Detach(GameObject target)
+	{
+		foreach (SpriteRenderer sprite in bossParts)
+		{
+			if (!sprite.GetComponent<Rigidbody2D>())
+			{
+				sprite.gameObject.AddComponent<Rigidbody2D>();
+			}
+			
+			sprite.GetComponent<Rigidbody2D>().isKinematic = false;
+			
+			if (sprite.GetComponent<Animator>())
+			{
+				sprite.GetComponent<Animator>().enabled = false;
+			}
+			sprite.GetComponent<PolygonCollider2D>().enabled = true;
+			
+			if(sprite.GetComponent<Attack>())
+			{
+				sprite.GetComponent<Attack>().enabled = false;
+			}
+			sprite.transform.DetachChildren();
+			
+		}
+		
+		transform.DetachChildren();  
+	}
+}
---
+++ Assets/Scripts/Boss/Golem.cs
@@ -0,0 +1,131 @@
+﻿public class Golem : BossManager {
+
+	//public Animator leftArm, rightArm;
+	
+	Timer LAttack1, RAttack1,
+	LAttack2, RAttack2;
+	
+	int totalSweeps = 3, currentSweeps = 0,
+	totalSlams = 4, currentSlams = 0;
+
+	//public List<Sprite> headStages, chestStages, lArmStages, rArmStages, c2, c3, c4;
+
+	void Start () {
+		
+		LAttack1 = gameObject.AddComponent<Timer>();
+		LAttack1.Setup("LeftHandAttack", 2, true);
+		
+		LAttack1.StartTimer();
+		
+		RAttack1 = gameObject.AddComponent<Timer>();
+		RAttack1.Setup("RightHandAttack", 2.5f, true);
+		RAttack1.enabled = false;
+		
+		LAttack2 = gameObject.AddComponent<Timer>();
+		LAttack2.Setup("LHandSlam", 1f, true);
+		
+		RAttack2 = gameObject.AddComponent<Timer>();
+		RAttack2.Setup("RHandSlam", 1f, true);
+	}
+
+	public override void StadgeOne ()
+	{
+
+	}
+	public override void StadgeTwo ()
+	{
+		bossParts[0].sprite = headStages[0];
+		bossParts[1].sprite = chestStages[0];
+		bossParts[2].sprite = c2[0];      
+		bossParts[3].sprite = c3[0];
+		bossParts[4].sprite = c4[0];
+		bossParts[5].sprite = rArmStages[0];
+		bossParts[6].sprite = lArmStages[0];
+	}
+	public override void StadgeThree ()
+	{
+
+	}
+	public override void StadgeFour ()
+	{
+		bossParts[0].sprite = headStages[1];
+		bossParts[1].sprite = chestStages[1];
+		bossParts[2].sprite = c2[1];
+		bossParts[3].sprite = c3[1];
+		bossParts[4].sprite = c4[1];
+		bossParts[5].sprite = rArmStages[1];
+		bossParts[6].sprite = lArmStages[1];
+	}
+	public override void StadgeFive ()
+	{
+		
+	}
+
+	public override void SetBossParts ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		if (currentSweeps <= totalSweeps)
+		{
+			if (LAttack1.complete)
+			{
+				currentSweeps++;
+				LAttack1.Reset();
+				leftArm.SetBool("Attack1", true);
+				
+				LAttack1.enabled = false;
+				
+				RAttack1.enabled = true;
+				RAttack1.StartTimer();
+			}
+			
+			if (RAttack1.complete)
+			{
+				currentSweeps++;
+				rightArm.SetBool("Attack1", true);
+				RAttack1.Reset();
+				RAttack1.enabled = false;
+				
+				LAttack1.enabled = true;
+				LAttack1.StartTimer();
+			}
+		}
+		else if (currentSlams <= totalSlams)
+		{
+			if (currentSlams == 0 && !LAttack2.active && !LAttack2.complete)
+			{
+				LAttack2.StartTimer();
+			}
+			if (LAttack2.complete)
+			{
+				currentSlams++;
+				LAttack2.Reset();
+				leftArm.SetBool("Attack2", true);
+				
+				LAttack2.enabled = false;
+				
+				RAttack2.enabled = true;
+				RAttack2.StartTimer();
+			}
+			
+			if (RAttack2.complete)
+			{
+				currentSlams++;
+				rightArm.SetBool("Attack2", true);
+				RAttack2.Reset();
+				RAttack2.enabled = false;
+				
+				LAttack2.enabled = true;
+				LAttack2.StartTimer();
+			}
+		}
+		else
+		{
+			currentSweeps = 0;
+			currentSlams = 0;
+		}
+	}
+}
