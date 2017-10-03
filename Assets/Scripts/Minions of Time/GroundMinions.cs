f4ba3eb6f8795ef0f18b596e9713103ba7d82630
Sean <Sean4.Taylor@live.uwe.ac.uk>
2017-10-03 13:47:31 +0100

    Getting basic templates for Minions
    Moved some folders around to seporate bosses and enemys
    Added 3 scripts
    MinionManager - functions all minions have
    GroundMinions - functions only ground minions have
    FlightMinions - functions only flying minions have
    Added a test scene
    Sean test sta(d)ge - copy of first level as to minimise any scene
    shareing and avoid damage to exsisting level

---
+++ Assets/Scripts/Minions of Time/GroundMinions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMinions : MinionsManager {
+
+    public float speed = 1.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody2D>().AddForce(transform.right * speed);
+    }
+}
---
+++ Assets/Scripts/Minions of Time/MinionsManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionsManager : MonoBehaviour {
+
+    byte health;
+    private bool alive
+    {
+        get
+        {
+            return health > 0;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        health = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!alive)
+        {
+            Death();
+        }
+	}
+
+    protected virtual void Death()
+    {
+        gameObject.SetActive(false);
+    }
+}
