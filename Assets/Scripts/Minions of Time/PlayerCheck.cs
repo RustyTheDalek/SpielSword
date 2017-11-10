f585c8a15dfc258ce102bd8aa45369ee44023b55
Sean <Sean4.Taylor@live.uwe.ac.uk>
2017-11-10 17:04:12 +0000

    Added template to find player
    Modified my scene - minor replacement of player
    Enemy.prefab - added a player checker sub object
    groundMinions - added the script to handle the checks for the player
    2D settings and tag manager - added two new layers for finding the
    player and the enemys on thier own
    Added PlayerCheck to tell the GroundMinion that the player is in fact
    near by

---
+++ Assets/Scripts/Minions of Time/PlayerCheck.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCheck : MonoBehaviour {
+
+    public GameObject parentMinion;
+    bool onEnter;
+    bool onExit;
+
+	// Use this for initialization
+	void Start () {
+        onEnter = false;
+        onExit = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        onExit = false;
+        if (!onEnter)
+        {
+            Debug.Log("I sense a player");
+            parentMinion.GetComponent<GroundMinions>().actPlayer = player.gameObject;
+            parentMinion.GetComponent<GroundMinions>().playerHere = true;
+        }
+        onEnter = true;
+    }
+
+    void OnTriggerExit2D(Collider2D player)
+    {
+        onEnter = false;
+        if (!onExit)
+        {
+            Debug.Log("Dont you want to play?");
+            parentMinion.GetComponent<GroundMinions>().playerHere = false;
+        }
+        onExit = true;
+    }
+}
