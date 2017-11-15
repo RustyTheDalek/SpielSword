8638310cc925d3b256675fef57c082285f0649cc
Ian Jones <enigma994@live.co.uk>
2017-11-15 11:06:46 +0000

    Files for Berserker Class

---
+++ Assets/Scripts/Villagers/Berserker.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Villager class that sacrfiices Speed(?) For Damage
+/// </summary>
+public class Berserker : Villager {
+}
