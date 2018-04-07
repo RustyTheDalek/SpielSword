39826bf5af86e3a7f301c53fc10b15996d157b9a
Ian <ianj@superfluousgames.co.uk>
2018-04-07 20:26:20 +0100

    Updated Rogue class
    -Changed logic of Rogue class so that it tests teleport with a circle cast instead of testing if the teleport is within the bounds of the Arena.

--- Assets/Scripts/Villagers/Rogue.cs
+++ Assets/Scripts/Villagers/Rogue.cs
@@ -1,16 +1,35 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// Script for Rogue Class, handles the teleporting of the player
+/// Created by : Ian Jones - 04/11/17
+/// Updated by : Ian Jones - 07/04/18
+/// </summary>
 public class Rogue : Villager {
 
+    /// <summary>
+    /// How far the Rogue can Teleport
+    /// </summary>
     public float distance = 5;
+
+    /// <summary>
+    /// Collision information for Teleport
+    /// </summary>
+    public RaycastHit2D teleportTest;
+
+    /// <summary>
+    /// How much to offset the height for circle cast as the Pivot for Villagers is at their f
+    /// </summary>
+    float yOffset = .5f;
+
+    /// <summary>
+    /// What layers count as collision for teleport
+    /// </summary>
+    public LayerMask collisionLayers;
 
 	// Use this for initialization
 	public override void Start ()
     {
-
         //Need to set Rogue animator here
         base.Start();
 
@@ -20,15 +39,22 @@ public class Rogue : Villager {
 
     public void Blink()
     {
+        //Figure out which was Rogue is facing
         float direction = melee.transform.position.x - transform.position.x;
 
-        Vector3 newPos = transform.position + Vector3.right * Mathf.Sign(direction) * distance;
+        //Test in that direction to see if the
+        teleportTest = Physics2D.CircleCast(transform.position + Vector3.up * yOffset, .5f, Vector3.right * Mathf.Sign(direction), distance, collisionLayers);
 
-        while (!GameManager.MoveRequest(PlayerCollisions, newPos))
+        Vector3 newPos;
+
+        //If a collider was hit then we need to use the position of the circle cast as the new position
+        if (teleportTest.collider)
         {
-            newPos = transform.position + Vector3.right * Mathf.Sin(direction) * distance --;
+            newPos = (Vector3)teleportTest.centroid;
+            Debug.Log("Didn't teleport full distance because: " + teleportTest.collider.name + " was in the way");
         }
-        Debug.Log("Teleport allowed");
+        else //Nothing was hit within the Distance so we can teleport the full distance
+        newPos = transform.position + Vector3.right * Mathf.Sign(direction) * distance;
 
         transform.position = newPos;
     }
