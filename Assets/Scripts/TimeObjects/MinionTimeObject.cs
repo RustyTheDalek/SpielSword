a6751a8e6613af928ee0cb32bb89dd7fbbd88b0b
Ian <ianj@superfluousgames.co.uk>
2018-07-18 02:01:27 +0100

    Fixed Minion Time Object not renabling animator

--- Assets/Scripts/TimeObjects/MinionTimeObject.cs
+++ Assets/Scripts/TimeObjects/MinionTimeObject.cs
@@ -35,6 +35,8 @@ public class MinionTimeObject : AnimatorTimeObject {
         {
             GetComponent<GroundMinions>().enabled = true;
         }
+
+        m_Anim.enabled = true;
     }
 
     private void DisableMinion()
