07bfafcb78190d87872fd3559d5836c6cf032824
Ian Jones <enigma994@live.co.uk>
2018-09-19 13:58:52 +0100

    Removed Slowdown

--- Assets/Scripts/TimeObjects/TimeObjectManager.cs
+++ Assets/Scripts/TimeObjects/TimeObjectManager.cs
@@ -136,7 +136,6 @@ public class TimeObjectManager : MonoBehaviour
         //}
         StartCoroutine(JumpTime());
         //Debug.Break();
-        Time.timeScale = .25f;
         timeState = TimeState.Backward;
     }
 }
