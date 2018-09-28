58edcf62c27a638e1db1bd74692cfd90332d34a3
Ian Jones <enigma994@live.co.uk>
2018-09-28 22:01:09 +0100

    Encasing more Ediitor scripts in Application.isPlaying

--- Assets/Editor/TimeObjectEditor.cs
+++ Assets/Editor/TimeObjectEditor.cs
@@ -10,22 +10,25 @@ class TimeObjectEditor : Editor
 
     private void OnSceneGUI()
     {
-        m_Target = (TimeObject)target;
-
-        GUIStyle textStyle = new GUIStyle()
+        if (Application.isPlaying)
         {
-            fontSize = 14,
-            alignment = TextAnchor.MiddleCenter
-        };
+            m_Target = (TimeObject)target;
 
-        textStyle.normal.background = GUI.skin.GetStyle("box").normal.background;   
-        textStyle.normal.textColor = Color.black;
+            GUIStyle textStyle = new GUIStyle()
+            {
+                fontSize = 14,
+                alignment = TextAnchor.MiddleCenter
+            };
 
-        Handles.Label(m_Target.transform.position + Vector3.up * 2.5f,
-            "Time State: " + m_Target.tObjectState.ToString() +
-            "\nTotal Frames: " + m_Target.TotalFrames +
-            "\nCurrent Frame: " + m_Target.currentFrame +
-            "\nStart Frame: " + m_Target.startFrame +
-            "\nFinish Frame: " + m_Target.finishFrame, textStyle);
+            textStyle.normal.background = GUI.skin.GetStyle("box").normal.background;
+            textStyle.normal.textColor = Color.black;
+
+            Handles.Label(m_Target.transform.position + Vector3.up * 2.5f,
+                "Time State: " + m_Target.tObjectState.ToString() +
+                "\nTotal Frames: " + m_Target.TotalFrames +
+                "\nCurrent Frame: " + m_Target.currentFrame +
+                "\nStart Frame: " + m_Target.startFrame +
+                "\nFinish Frame: " + m_Target.finishFrame, textStyle);
+        }
     }
 }
